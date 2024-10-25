19517584200cf446a80d5db1aa8ab4b168bed063
sunxiaofei <sunxiaofei1422@gmail.com>
2024-10-25 12:50:28 +0800

    Site updated: 2024-10-25 12:50:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/24/hello-world/","2024/10/25/newpost/"];function toRandomPost(){
+var posts=["2024/10/24/hello-world/","2024/10/25/newpost-1/","2024/10/25/newpost/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
