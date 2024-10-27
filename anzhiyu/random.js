b5102319979c6dbcb7fd0a6e189e21eeb93df7c2
sunxiaofei <sunxiaofei1422@gmail.com>
2024-10-27 21:34:33 +0800

    Site updated: 2024-10-27 21:34:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/26/快速搭建个人博客：Hexo-GitHub-Pages-Cloudflare-Pages-完整指南/"];function toRandomPost(){
+var posts=["2024/10/27/telegraph-Image：搭建你的专属开源图床/","2024/10/26/快速搭建个人博客：Hexo-GitHub-Pages-Cloudflare-Pages-完整指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
