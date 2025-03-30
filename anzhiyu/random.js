f4719b89dcc8e549da39230ef7eb373094a2c30c
sunxiaofei <sunxiaofei1422@gmail.com>
2025-03-30 16:43:51 +0800

    Site updated: 2025-03-30 16:43:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/05/域名免费拿！一分钟学会-us-kg-域名申请并配置到-Cloudflare/","2024/10/29/突破次元壁！轻松上手 VPN 节点搭建/","2024/10/29/如何使用Google-Email发送邮件/","2024/10/26/快速搭建个人博客：Hexo-GitHub-Pages-Cloudflare-Pages-完整指南/"];function toRandomPost(){
+var posts=["2024/10/29/突破次元壁！轻松上手 VPN 节点搭建/","2024/10/29/如何使用Google-Email发送邮件/","2024/10/26/快速搭建个人博客：Hexo-GitHub-Pages-Cloudflare-Pages-完整指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
